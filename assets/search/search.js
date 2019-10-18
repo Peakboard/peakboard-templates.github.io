ddf7569d9090a0350b477328d2ceeb5e60699cdd
RealTechErwin <me@erwinlengkeek.nl>
2019-10-18 17:32:46 +0800

    Reset searchbox value when category is clicked

--- assets/search/search.js
+++ assets/search/search.js
@@ -42,7 +42,7 @@ var main = {
         // render method for searchBox
         const customSearchBox = instantsearch.connectors.connectSearchBox(
             (renderOptions, isFirstRender) => {
-                const { query, refine, widgetParams } = renderOptions;
+                const { query, refine, clear, widgetParams } = renderOptions;
 
                 if (isFirstRender) {
                     // create and set attributes on form container
@@ -96,8 +96,18 @@ var main = {
                     button.setAttribute('type', 'submit');
                     button.innerHTML = '<svg class="ais-SearchBox-submitIcon" xmlns="http://www.w3.org/2000/svg" width="10" height="10" viewBox="0 0 40 40"> <path d="M26.804 29.01c-2.832 2.34-6.465 3.746-10.426 3.746C7.333 32.756 0 25.424 0 16.378 0 7.333 7.333 0 16.378 0c9.046 0 16.378 7.333 16.378 16.378 0 3.96-1.406 7.594-3.746 10.426l10.534 10.534c.607.607.61 1.59-.004 2.202-.61.61-1.597.61-2.202.004L26.804 29.01zm-10.426.627c7.323 0 13.26-5.936 13.26-13.26 0-7.32-5.937-13.257-13.26-13.257C9.056 3.12 3.12 9.056 3.12 16.378c0 7.323 5.936 13.26 13.258 13.26z"></path> </svg>';
 
+                    // create and set attributes on reset button
+                    const resetButton = document.createElement('button');
+                    resetButton.setAttribute('type', 'reset');
+                    resetButton.setAttribute('id', 'resetSearchBox');
+
+                    resetButton.addEventListener('click', () => {
+                        clear();
+                    });
+
                     form.appendChild(input);
                     form.appendChild(button);
+                    form.appendChild(resetButton);
                 }
 
                 widgetParams.container.querySelector('input').value = query;
@@ -200,6 +210,9 @@ var main = {
                 element.addEventListener('click', function (event) {
                     event.preventDefault();
 
+                    // reset search if any
+                    document.getElementById('resetSearchBox').click();
+
                     if (event.currentTarget.dataset.value === document.getElementById('featured-category').innerHTML) {
                         removeClass(document.getElementById('carousel'), 'hidden');
                         addClass(document.getElementById('hits-container'), 'mt-8');
@@ -286,6 +299,9 @@ var main = {
             _toConsumableArray(widgetParams.container.querySelectorAll('a')).forEach(function (element) {
                 element.addEventListener('click', function (event) {
                     event.preventDefault();
+
+                    // reset search if any
+                    document.getElementById('resetSearchBox').click();
 
                     if(allCategory === event.currentTarget.dataset.value) {
                         refine(' ');
