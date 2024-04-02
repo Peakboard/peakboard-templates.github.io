da8cc6dba3c92c8786507945f6a4b4b28f3c1678
Alexander König <alexander.koenig@peakboard.com>
2024-04-02 15:13:16 +0200

    Fair Templates Removed as they were moved to the Projects Repository

--- _templates/_Fair_Templates/Scripts_Backup/Single_Seat_1.lua
+++
@@ -1,6 +0,0 @@
-for index1 = 0, data.Machine_Status.count-1 do		
-    local activeStatus = data.Machine_Status[index1].Active
-    if activeStatus then
---			peakboard.log('Error Code: ' .. data.Machine_Status[index1].Code .. ' – ' .. data.Machine_Status[index1].Text)
-    end
-end
